c5764f7a995a6b2adfc6a0cc3b64d2dc31c91a81
vkcodes005 <vaishnavikashyap005@gmail.com>
2026-06-13 15:02:36 +0530

    Add files via upload

---
+++ Sum2Num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x,y;
+    cout<<"Enter your first number:";
+    cin>>x;
+    cout<<"Enter your Second numbr:";
+    cin>>y;
+    cout<<x+y;
+    return 0;
+}
