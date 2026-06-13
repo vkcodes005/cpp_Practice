a8f06749b1cbc2f39bc16a893fadcfb2b2216bba
vkcodes005 <vaishnavikashyap005@gmail.com>
2026-06-13 15:01:12 +0530

    Add files via upload

---
+++ NewLine.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Khushi\n";
+    cout<<"Hello Papa\n";
+    cout<<"Hello Mummy\n";
+    cout<<"Hello Munchun"<<endl;
+    cout<<"Hello Pritam\n";
+    cout<<"Hello Kukko"<<endl;
+    cout<<"Thank you\n";
+    return 0;
+}
