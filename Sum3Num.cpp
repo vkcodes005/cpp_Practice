37ca3baccd4f310800721489e5cf7bf1a05abbd1
vkcodes005 <vaishnavikashyap005@gmail.com>
2026-06-13 15:03:13 +0530

    Add files via upload

---
+++ Sum3Num.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"Enter 1st number:";
+    cin>>x;
+    cout<<"Enter 2nd number";
+    cin>>y;
+    cout<<"Enter 3rd number";
+    cin>>z;
+    cout<<x+y+z;
+    return 0;
+}
---
+++ VolumeSphere.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter the Radius:";
+    cin>>r;
+    float v = (4.0/3)*3.14*(r*r*r);
+    cout<<v;
+    return 0;
+}
